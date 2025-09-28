5172dbdf8f5c9a8357a9baef1a16999ead405367
srinithi5 <srinithi.e.18@gmail.com>
2025-09-28 17:59:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Task from './Task.js';
+
+function App() {
+  return (
+    <div>
+      <Task/>
+    </div>
+  );
+}
+
+export default App;
